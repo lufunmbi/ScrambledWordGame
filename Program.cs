52c30e8a74682a73a8cbc2dd689b54850751743f
Funmbi <bammieyour@gmail.com>
2020-02-15 23:53:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using ScrammbledWordGame.Data;
+using ScrammbledWordGame.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrammbledWordGame
+{
+    class Program
+    {
+        private static readonly FileReader _fileReader = new FileReader();
+        private static readonly WordMatcher _wordMatcher = new WordMatcher();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool continueGame = true;
+                do
+                {
+                    Console.WriteLine(Constants.OptionsOnHowToEnterScrambledWords);
+
+                    var chosenMode = Console.ReadLine() ?? string.Empty;
+
+                    switch (chosenMode.ToUpper())
+                    {
+                        case Constants.File:
+                            Console.Write(Constants.EnterScrambledWordsViaFile);
+                            ExecuteFileScrambledWords();
+                            break;
+                        case Constants.Manual:
+                            Console.Write(Constants.EnterScrambledWordsManually);
+                            ExecuteManualScrambledWord();
+                            break;
+                        default:
+                            Console.WriteLine(Constants.EnterScrambledWordsOptionNotRecognized);
+                            break;
+                    }
+                    var continueDecision = string.Empty;
+                    do
+                    {
+                        Console.WriteLine(Constants.OptionsOnContinuingTheProgram);
+                        continueDecision = (Console.ReadLine() ?? string.Empty);
+
+                    } while (
+                    !continueDecision.Equals(Constants.Yes, StringComparison.OrdinalIgnoreCase) &&
+                    !continueDecision.Equals(Constants.No, StringComparison.OrdinalIgnoreCase));
+
+                    continueGame = continueDecision.Equals(Constants.Yes, StringComparison.OrdinalIgnoreCase);
+
+                }
+                while (continueGame);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(Constants.ErrorProgramWillBeTermminated + ex.Message);
+            }
+
+        }
+
+        private static void ExecuteManualScrambledWord()
+        {
+            try
+            {
+                var manualValues = Console.ReadLine() ?? string.Empty;
+                string[] scrambledWords = manualValues.Split(',');
+                DisplayMatchedUnscrambledWords(scrambledWords);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(Constants.ErrorScrambledWordsCannotBeLoaded + ex.Message);
+            }
+            
+        }
+
+        private static void ExecuteFileScrambledWords()
+        {
+            var fileName = Console.ReadLine() ?? string.Empty;
+            string[] scrambledWords = _fileReader.Read(fileName);
+            DisplayMatchedUnscrambledWords(scrambledWords);
+        }
+
+        private static void DisplayMatchedUnscrambledWords(string[] scrambledWords)
+        {
+            string[] wordList = _fileReader.Read(Constants.WordListFilename);
+
+            List<MatchedWord> matchedWords = _wordMatcher.Match(scrambledWords, wordList);
+
+            if (matchedWords.Any())
+            {
+                foreach (var mw in matchedWords)
+                {
+                    Console.WriteLine(Constants.MatchFound, mw.ScrambledWord, mw.Word);
+                }
+            }
+            else
+            {
+                Console.WriteLine(Constants.MatchNotFound);
+            }
+        }
+    }
+}
---
+++ Workers/WordMatcher.cs
@@ -0,0 +1,62 @@
+﻿using ScrammbledWordGame.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrammbledWordGame.Workers
+{
+    public class WordMatcher
+    {
+        public List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
+        {
+            var matchedWords = new List<MatchedWord>();
+
+            foreach (var scrambledWord in scrambledWords)
+            {
+                foreach(var word in wordList)
+                {
+                    if(scrambledWord.Equals(word, StringComparison.OrdinalIgnoreCase))
+                    {
+                        matchedWords.Add(BuildMatchedWord(scrambledWord, word));
+                    }
+                    else
+                    {
+                        var scrambledWordArray = scrambledWord.ToCharArray();
+                        var wordArray = word.ToCharArray();
+
+                        Array.Sort(scrambledWordArray);
+                        Array.Sort(wordArray);
+
+                        var sortedScrambledWord = new string(scrambledWordArray);
+                        var sortedWord = new string(wordArray);
+
+                        if(sortedScrambledWord.Equals(sortedWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            matchedWords.Add(BuildMatchedWord(scrambledWord, word));
+                        }
+                    }
+                }
+            }
+            return matchedWords;
+        }
+        //Helper Class is created so that we dont have to write all this code in the add method above. Just keeping the code simple and sweet
+        private MatchedWord BuildMatchedWord(string scrambledWord, string word)
+        {
+            MatchedWord matchedword = new MatchedWord
+            {
+                ScrambledWord = scrambledWord,
+                Word = word
+            };
+
+
+            //MatchedWord matchedword = new MatchedWord();
+            //matchedword.ScrambledWord = scrambledWord;
+            //matchedword.Word = word;
+
+            return matchedword;
+        }
+    }
+}
+;
